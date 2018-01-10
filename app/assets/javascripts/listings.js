fdf783cc8a968c0cdd75afb6eb1a31ae49c97b12
cheahhowong <cheahhowong@hotmail.com>
2018-01-10 18:51:51 +0800

    standardized environment config in development and production

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-let wrap = $("#listing_profile_right");
+var wrap = $("#listing_profile_right");
 
 wrap.on("scroll", function(e) {
     
@@ -13,8 +13,8 @@ wrap.on("scroll", function(e) {
   
 });
 
-let main = function(){
-   let menu = $('#listing_profile_right');
+var main = function(){
+   var menu = $('#listing_profile_right');
 
    $(document).scroll(function(){
       if ( $(this).scrollTop() >= $(window).height() - menu.height() ){
